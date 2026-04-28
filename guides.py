5eb70cf84ad53d293456ef784d0b246f9c2716ba
Fabiola <fabiola.a.chazarreta@gmail.com>
2026-04-28 18:26:16 -0300

    Reorganazing repo for edd and poo

--- guides.py
+++
@@ -1,8 +0,0 @@
-# start up file
-
-x = 7
-y = x + 1 # y = 8
-y = y + 2 # y = 10
-z = x + 2 * y # z = 7 + 2 * 10 = 27 multiplication has priority?
-z = z - 1 # z = 26
-print("Final value of z is", z)
